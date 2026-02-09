3b448e8c5d763d02f35b39d1cc9850301f3863cc
Gareja Bharat <garejabharatk@gmail.com>
2026-02-09 22:55:12 +0530

    RTK code

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "../futures/apislice";
+const store=configureStore({
+reducer:{
+  [api.reducerPath]:api.reducer
+},
+middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(api.middleware)
+})
+
+export default store;
